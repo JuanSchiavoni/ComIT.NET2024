121d2d0c6455477e563abe7560e4fe6a2a45b85d
Juan <juanschiavoni713@gmail.com>
2024-10-15 11:03:55 -0300

    ABM Razor

---
+++ CursosApp/Entities/Profesor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComIT.CursosApp.Entities
+{
+    public class Profesor : Persona
+    {
+        public int AñosExp = 0;
+
+        public Profesor(long dni, string nombre, string apellido, DateOnly fechaNac, int añosExp) 
+            : base(dni, nombre, apellido,fechaNac)
+        {
+            AñosExp = añosExp;
+        }
+
+        public override string DevolverNombreCompleto()
+        {
+            return $"Profesor: {base.DevolverNombreCompleto()}";
+        }
+
+        public override string Saludo()
+        {
+            return "Buenos días alumnos";
+        }
+    }
+}
