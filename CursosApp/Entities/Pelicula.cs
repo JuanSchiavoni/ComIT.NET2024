127b92309cd800c187f2dd1d79fd9c8ff09a9bd5
Juan <juanschiavoni713@gmail.com>
2024-10-17 07:12:15 -0300

    Ejercicio Blazor 2

---
+++ CursosApp/Entities/Pelicula.cs
@@ -0,0 +1,14 @@
+﻿namespace ComIT.CursosApp.Entities
+{
+    public class Pelicula
+    {
+        public string name = string.Empty;
+        public int calif = 0;
+
+        public Pelicula (string name, int calif)
+        {
+            this.name = name;
+            this.calif = calif;
+        }
+    }
+}
