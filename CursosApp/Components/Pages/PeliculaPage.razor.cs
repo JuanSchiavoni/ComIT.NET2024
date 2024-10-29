5efd39e025f96f81250ccd1a944527a7c6b5d582
Juan <juanschiavoni713@gmail.com>
2024-10-29 18:40:40 -0300

    Componentes

---
+++ CursosApp/Components/Pages/PeliculaPage.razor.cs
@@ -0,0 +1,65 @@
+﻿using ComIT.CursosApp.Entities;
+
+namespace CursosApp.Components.Pages
+{
+    public partial class PeliculaPage
+    {
+        private string error = "";
+        private bool estamosModificando = false;
+        private Pelicula? pelimodi;
+
+        private string nombrePeli = "";
+        private int calificacion = 0;
+
+        private List<Pelicula> PeliculasList = new();
+
+        private void AgregarPeli()
+        {
+            if (nombrePeli == "" || calificacion == null)
+            {
+                error = "No se cargaron todos los datos";
+            }
+            else
+            {
+                PeliculasList.Add(new Pelicula(nombrePeli, calificacion));
+
+                nombrePeli = "";
+                calificacion = 0;
+            }
+        }
+
+        private void Modificar(Pelicula pelimodificar)
+        {
+            estamosModificando = true;
+            pelimodi = pelimodificar;
+
+            nombrePeli = pelimodificar.name;
+            calificacion = pelimodificar.calif;
+        }
+
+        private void GuardarCambios()
+        {
+            if (nombrePeli == "" || calificacion == null)
+            {
+                error = "No se cargaron todos los datos";
+            }
+            else
+            {
+                pelimodi.name = nombrePeli;
+                pelimodi.calif = calificacion;
+
+                nombrePeli = "";
+                calificacion = 0;
+
+                estamosModificando = false;
+                pelimodi = null;
+            }
+        }
+
+        private void Eliminar(Pelicula peliEliminiar)
+        {
+            PeliculasList.Remove(peliEliminiar);
+        }
+
+    }
+}
---
+++ CursosApp/Components/Pages/Profesores/ProfesorPage.razor.cs
@@ -0,0 +1,93 @@
+﻿using ComIT.Comunes.Entities;
+
+
+
+namespace CursosApp.Components.Pages.Profesores
+{
+    public partial class ProfesorPage
+    {
+        private string error = "";
+        private bool estamosModificando = false;
+        private Profesor? profesorModificando;
+
+        private long dni = 0;
+        private string nombre = "";
+        private string apellido = "";
+        private DateOnly? fechaNac;
+        private int añosExp;
+        private string materia = "";
+        private List<Profesor> ProfesoresList = new();
+
+        private void NuevoProfesor()
+        {
+
+        }
+
+        private void Agregar()
+        {
+            if (dni == 0 || nombre == "" || apellido == "" || fechaNac == null || materia == "")
+            {
+                error = "No se ingresaron todos los datos";
+            }
+            else
+            {
+                ProfesoresList.Add(new Profesor(dni, nombre, apellido, (DateOnly)fechaNac, añosExp, materia));
+
+                dni = 0;
+                nombre = "";
+                apellido = "";
+                fechaNac = null;
+                añosExp = 0;
+                materia = "";
+
+                error = "";
+            }
+        }
+
+        private void Modificar(Profesor profeModificar)
+        {
+            estamosModificando = true;
+            profesorModificando = profeModificar;
+
+            dni = profeModificar.DNI;
+            nombre = profeModificar.Nombre;
+            apellido = profeModificar.Apellido;
+            fechaNac = profeModificar.FechaNac;
+            añosExp = profeModificar.AñosExp;
+            materia = profeModificar.Materia;
+        }
+
+        private void GuardarCambios()
+        {
+            if (dni == 0 || nombre == "" || apellido == "" || fechaNac == null || materia == "")
+            {
+                error = "No se ingresaron todos los datos";
+            }
+            else
+            {
+                profesorModificando.DNI = dni;
+                profesorModificando.Nombre = nombre;
+                profesorModificando.Apellido = apellido;
+                profesorModificando.FechaNac = (DateOnly)fechaNac;
+                profesorModificando.AñosExp = añosExp;
+                profesorModificando.Materia = materia;
+
+                dni = 0;
+                nombre = "";
+                apellido = "";
+                fechaNac = null;
+                añosExp = 0;
+                materia = "";
+
+                estamosModificando = false;
+                profesorModificando = null;
+            }
+        }
+
+        private void Eliminar(Profesor profeEliminar)
+        {
+            ProfesoresList.Remove(profeEliminar);
+        }
+        
+    }
+}
